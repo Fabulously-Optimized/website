ca2ca609755eb9263124f214393f6e7a09711e5c
Madis Otenurm <Madis0@users.noreply.github.com>
2025-03-31 22:29:04 +0200

    Update redirects.ts

--- redirects.ts
+++ redirects.ts
@@ -20,6 +20,8 @@ export const redirects = [
   ["/terms", "https://wiki.download.fo/readme/disclaimers"],
   ["/translate", "https://wiki.download.fo/readme/language-support"],
   ["/thanks", "/contribute"],
+  ["/donate", "/contribute"],
+  ["/server", "https://wiki.download.fo/server-setup"],
   ["/update", "https://wiki.download.fo/readme/update-instructions"],
   ["/wiki", "https://wiki.download.fo"]
 ]
